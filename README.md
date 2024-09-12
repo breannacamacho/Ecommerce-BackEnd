8883057f9e347be018faf1b47b4ae5067a7d5521
Breanna Camacho <breannacamacho1@gmail.com>
2024-09-11 23:25:50 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# E-Commerce Backend
+
+## Description
+
+This project demonstrates how to build the back end for an e-commerce site using modern technologies like Express.js, PostgreSQL, and Sequelize ORM. The back end includes functionality for managing categories, products, and tags, as well as performing CRUD operations (Create, Read, Update, Delete) via API routes.
+
+## Table of Contents 
+
+- [Badges](#badges)
+- [Contribute](#contribute)
+- [Questions/Contact](#questions)
+- [License](#license)
+
+## Badges
+
+[![Static Badge](https://img.shields.io/badge/GitHub-breannacamacho-darkgreen)](https://github.com/breannacamacho) 
+[![Static Badge](https://img.shields.io/badge/Inquirer-8.2.4-blue)](https://www.npmjs.com/package/inquirer) 
+[![Static Badge](https://img.shields.io/badge/Jest-29.7.0-red)](https://www.npmjs.com/package/jest) 
+
+## Contribute
+
+Contributions are what make the open-source community such a wonderful place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that could improve this project, please fork the repo and create a pull request. You can also open an issue with the tag "enhancement".
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+## Questions
+
+[Link to GitHub Profile](https://github.com/breannacamacho)
+
+If you have any additional questions, feel free to reach out via email at breannacamacho1@gmail.com.
+
+## License
+
+N/A
---
+++ routes/api/category-routes.js
@@ -0,0 +1,80 @@
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+// The `/api/categories` endpoint
+
+// find all categories. Includes its associated Products
+router.get("/", async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  // find one category by its `id` value. Includes its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with this id!" });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  // creates a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  // updates a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  // deletes a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with this id!" });
+      return;
+    }
+
+    res.status(200).json("Category has been deleted!");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
